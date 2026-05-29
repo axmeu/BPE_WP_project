daa06a504ccfe3bda04ab90861304c78ecf05f7a
axmeu <159260349+axmeu@users.noreply.github.com>
2026-05-29 14:23:40 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,9 @@
 
 curl -fsSL https://pixi.sh/install.sh | bash
 echo 'export PATH="$HOME/.pixi/bin:$PATH"' >> ~/.bashrc
-export PATH="$HOME/.pixi/bin:$PATH"
+echo 'export PATH="$HOME/.pixi/bin:$PATH"' >> ~/.profile
+echo 'export PATH="$HOME/.pixi/bin:$PATH"' >> ~/.bash_profile
 
 cd ~/work
+pixi install
 pixi run python -m ipykernel install --user --name pixi-env --display-name "pixi-env"
